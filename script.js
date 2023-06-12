994b7345ed8d87fa995a35e51c993c3e3ccf8cfb
AishaSK <aishasuhail2005@gmail.com>
2023-06-12 17:59:14 +0400

    Added random,eraser and reset option

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black"
+
 const size16 = document.querySelector('#s16')
 const size32 = document.querySelector('#s32')
 const size64 = document.querySelector('#s64')
@@ -29,12 +31,30 @@ function createBoard(size) {
     let numDivs = size * size
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div")
-        div.addEventListener("mouseover", function(){
-            div.style.backgroundColor = "black"
-        })
-        div.style.backgroundColor = "yellow"
+        div.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend", div)
 
     }
 }
 
+function colorDiv(){
+    //if(click){}
+    if (color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else if(color == "white"){
+        this.style.backgroundColor = 'white'
+    }
+    else {
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
+}
